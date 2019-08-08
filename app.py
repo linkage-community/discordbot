9248bb75c4c765392d35402c65a63105640b3f8d
otofune <otofune@otofune.me>
2019-08-09 01:00:44 +0900

    :traffic_light:

--- app.py
+++ app.py
@@ -14,19 +14,30 @@ async def send(message: str):
             })
 
 
-async def join_member(member, channel):
-    await send('[{}] {} was joined.'.format(channel, member))
+async def join_member(member_name, channel):
+    await send('[{}] {} was joined.'.format(channel, member_name))
+async def leave_member(member_name, channel):
+    await send('[{}] {} was left.'.format(channel, member_name))
+async def move_member(member_name, before_channel, after_channel):
+    await send('[{}] {} was moved from {}.'.format(after_channel, member_name, after_channel))
 
-async def leave_member(member, channel):
-    await send('[{}] {} was left.'.format(channel, member))
+async def handle_channel_change(member, before_channel, after_channel):
+    # block any case without changing channel
+    if before_channel == after_channel:
+        return
+
+    member_name = member.nick or str(member)
+
+    if before_channel is None:
+        await join_member(member_name, after_channel)
+        return
+    if after_channel is None:
+        await leave_member(member_name, before_channel)
+        return
+    await move_member(member_name, before_channel, after_channel)
 
 @client.event
 async def on_voice_state_update(member, before, after):
-    if before.channel is None:
-        await join_member(member, after.channel)
-        return
-    if after.channel is None:
-        await leave_member(member, before.channel)
-        return
+    await handle_channel_change(member, before.channel, after.channel)
 
 client.run(DISCORD_TOKEN)
